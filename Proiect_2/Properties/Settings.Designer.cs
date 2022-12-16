35820227e93f60a12bc236113699ace4f712f0d7
HIrina27 <102599848+HIrina27@users.noreply.github.com>
2022-12-16 13:11:24 +0200

    Baza de date

--- Proiect_2/Properties/Settings.Designer.cs
+++ Proiect_2/Properties/Settings.Designer.cs
@@ -8,21 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Proiect_2.Properties
-{
+namespace Proiect_2.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-N4DCGIN;Initial Catalog=ProiectPBD;Integrated Security=True")]
+        public string ProiectPBDConnectionString {
+            get {
+                return ((string)(this["ProiectPBDConnectionString"]));
             }
         }
     }
